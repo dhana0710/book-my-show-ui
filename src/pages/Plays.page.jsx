e640ec236338eba1732bbee69ca5eb9839d0e91d
dhana0710 <dhanasekar9905@gmail.com>
2021-07-20 01:50:47 +0530

    add Plays disclosure

--- src/pages/Plays.page.jsx
+++ src/pages/Plays.page.jsx
@@ -1,54 +1,65 @@
 import React from 'react'
 
 
+
 //component
 import Poster from '../components/Poster/poster.component';
+import PlaysFilter from '../components/PlaysFilters/PlaysFilters.component';
 
 
 const Plays = () => {
     return (
         <>
             <div className="container mx-auto px-4 lg:px-20">
-                <h2 className="text-xl md:text-2xl font-bold">Plays in Chennai</h2>
+
                 <div className="w-full lg:flex lg:flex-row-reverse">
-                    <div className="flex flex-wrap lg:w-7/12">
-                        <div className="w-1/2 md:w-1/3 my-4 lg:my-8">
-                            <Poster
-                                src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNyBKdWw%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00309696-fsqhgswrqc-portrait.jpg"
-                                title="`Kanthamathi` (Sadarame) Surabhi Theatre Play"
-                                subtitle="Telugu -> ₹400"
-                            />
-                        </div>
-                        <div className="w-1/2 md:w-1/3 my-4 lg:my-8">
-                            <Poster
-                                src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNyBKdWw%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00309696-fsqhgswrqc-portrait.jpg"
-                                title="`Kanthamathi` (Sadarame) Surabhi Theatre Play"
-                                subtitle="Telugu -> ₹400"
-                            />
-                        </div>
-                        <div className="w-1/2 md:w-1/3 my-4 lg:my-8">
-                            <Poster
-                                src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNyBKdWw%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00309696-fsqhgswrqc-portrait.jpg"
-                                title="`Kanthamathi` (Sadarame) Surabhi Theatre Play"
-                                subtitle="Telugu -> ₹400"
-                            />
-                        </div>
-                        <div className="w-1/2 md:w-1/3 my-4 lg:my-8">
-                            <Poster
-                                src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNyBKdWw%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00309696-fsqhgswrqc-portrait.jpg"
-                                title="`Kanthamathi` (Sadarame) Surabhi Theatre Play"
-                                subtitle="Telugu -> ₹400"
-                            />
-                        </div>
-                        <div className="w-1/2 md:w-1/3 my-4 lg:my-8">
-                            <Poster
-                                src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNyBKdWw%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00309696-fsqhgswrqc-portrait.jpg"
-                                title="`Kanthamathi` (Sadarame) Surabhi Theatre Play"
-                                subtitle="Telugu -> ₹400"
-                            />
+                    <div className="lg:w-3/4">
+                        <h2 className="text-xl md:text-2xl font-bold mb-4">Plays in Chennai</h2>
+                        <div className="flex flex-wrap">
+                            <div className="w-1/2 md:w-1/3 lg:w-1/4 my-3 ">
+                                <Poster
+                                    src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNyBKdWw%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00309696-fsqhgswrqc-portrait.jpg"
+                                    title="`Kanthamathi` (Sadarame) Surabhi Theatre Play"
+                                    subtitle="Telugu -> ₹400"
+                                />
+                            </div>
+                            <div className="w-1/2 md:w-1/3 lg:w-1/4 my-3 ">
+                                <Poster
+                                    src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNyBKdWw%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00309696-fsqhgswrqc-portrait.jpg"
+                                    title="`Kanthamathi` (Sadarame) Surabhi Theatre Play"
+                                    subtitle="Telugu -> ₹400"
+                                />
+                            </div>
+                            <div className="w-1/2 md:w-1/3 lg:w-1/4 my-3 ">
+                                <Poster
+                                    src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNyBKdWw%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00309696-fsqhgswrqc-portrait.jpg"
+                                    title="`Kanthamathi` (Sadarame) Surabhi Theatre Play"
+                                    subtitle="Telugu -> ₹400"
+                                />
+                            </div>
+                            <div className="w-1/2 md:w-1/3 lg:w-1/4 my-3 ">
+                                <Poster
+                                    src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNyBKdWw%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00309696-fsqhgswrqc-portrait.jpg"
+                                    title="`Kanthamathi` (Sadarame) Surabhi Theatre Play"
+                                    subtitle="Telugu -> ₹400"
+                                />
+                            </div>
+                            <div className="w-1/2 md:w-1/3 lg:w-1/4 my-3 ">
+                                <Poster
+                                    src="https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:ote-U2F0LCAxNyBKdWw%3D,ots-29,otc-FFFFFF,oy-612,ox-24/et00309696-fsqhgswrqc-portrait.jpg"
+                                    title="`Kanthamathi` (Sadarame) Surabhi Theatre Play"
+                                    subtitle="Telugu -> ₹400"
+                                />
+                            </div>
                         </div>
                     </div>
-                    <div></div>
+                    <div className="lg:w-1/4">
+                        <h2 className="text-xl md:text-2xl font-bold mb-4">Filters</h2>
+                        <div>
+                            <PlaysFilter title="Date" tags={["Today", "Tomorrow"]} />
+                            <PlaysFilter title="Language" tags={["Tamil", "English", "Telegu"]} />
+                        </div>
+                    </div>
                 </div>
 
 
